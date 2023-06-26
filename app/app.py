33df52f183cdcddb9bb00a3ec1ab4eb15243ca91
Anna Benke <anna.benke@gmail.com>
2023-06-26 15:00:18 +0200

    Polish

--- app/app.py
+++ app/app.py
@@ -10,17 +10,17 @@ repository = services.DataRepository(cache)
 
 @app.route('/info/')
 def info():
-    pd_info = repository.get_info()
+    result = repository.get_info()
     return Response(
-       pd_info.to_json(orient='records'),
+       result.to_json(orient='records'),
        mimetype='application/json')
 
 
 @app.route('/aircrafts/models/')
 def aircrafts_models():
-    pd_info = repository.get_aircraft_models()
+    result = repository.get_aircraft_models()
     return Response(
-       pd_info.to_json(orient='records'),
+       result.to_json(orient='records'),
        mimetype='application/json')
 
 
@@ -28,28 +28,27 @@ def aircrafts_models():
 def aircrafts_filtered():
     manufacturer = request.args.get('manufacturer')
     model = request.args.get('model')
-    pd_info = repository.get_aircrafts_filtered(manufacturer, model)
+    result = repository.get_aircrafts_filtered(manufacturer, model)
     return Response(
-       pd_info.to_json(orient='records'),
+       result.to_json(orient='records'),
        mimetype='application/json')
 
 
 @app.route('/aircrafts/reports/')
 def aircrafts_report():
-    pd_info = repository.get_report()
+    result = repository.get_report()
     return Response(
-       pd_info.to_json(orient='records'),
+       result.to_json(orient='records'),
        mimetype='application/json')
 
 
 @app.route('/aircrafts/reports/pivot/')
 def aircrafts_report_pivot():
-    pd_info = repository.get_report_pivot()
+    result = repository.get_report_pivot()
     return Response(
-       pd_info.to_json(orient='records'),
+       result.to_json(orient='records'),
        mimetype='application/json')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
