707ecfd8152f98d27da3f6e5d9cf271bed3fb39f
Anna Benke <anna.benke@gmail.com>
2023-06-26 18:06:55 +0200

    Type hints

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ repository = services.faa_data_repository_factory(app.config['FFA_DATA_DIR_PATH'
 
 
 @app.route('/info/')
-def info():
+def info() -> Response:
     result = repository.get_info()
     return Response(
        result.to_json(orient='records'),
@@ -17,7 +17,7 @@ def info():
 
 
 @app.route('/aircrafts/models/')
-def aircrafts_models():
+def aircrafts_models() -> Response:
     result = repository.get_aircraft_models()
     return Response(
        result.to_json(orient='records'),
@@ -25,7 +25,7 @@ def aircrafts_models():
 
 
 @app.route('/aircrafts/')
-def aircrafts_filtered():
+def aircrafts_filtered() -> Response:
     manufacturer = request.args.get('manufacturer')
     model = request.args.get('model')
     result = repository.get_aircrafts_filtered(manufacturer, model)
@@ -35,7 +35,7 @@ def aircrafts_filtered():
 
 
 @app.route('/aircrafts/reports/')
-def aircrafts_report():
+def aircrafts_report() -> Response:
     result = repository.get_report()
     return Response(
        result.to_json(orient='records'),
@@ -43,7 +43,7 @@ def aircrafts_report():
 
 
 @app.route('/aircrafts/reports/pivot/')
-def aircrafts_report_pivot():
+def aircrafts_report_pivot() -> Response:
     result = repository.get_report_pivot()
     return Response(
        result.to_json(orient='records'),
@@ -51,7 +51,7 @@ def aircrafts_report_pivot():
 
 
 @app.route('/sql/', methods=['POST'])
-def data_by_sql_string():
+def data_by_sql_string() -> Response:
     result = repository.get_data_by_sql_string(request.json['sql'])
     return Response(
        result.to_json(orient='records'),
@@ -59,4 +59,7 @@ def data_by_sql_string():
 
 
 if __name__ == '__main__':
+    """Using DEV setup.
+    I would use Nginx and Gunicorn for production setup and follow this https://testdriven.io/blog/dockerizing-flask-with-postgres-gunicorn-and-nginx/?fbclid=IwAR38OY8HqFiu_wsTk7vfTsLfTaK7dUk4pxfGhQiRr82nqq1CSP6nPzEp48w.
+    """
     app.run(host='0.0.0.0', port=5000, debug=True)
--- app/services.py
+++ app/services.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 import os
 import pandas as pd
 from pandasql import sqldf
 from pyarrow import parquet as pq
 
 
-def faa_data_repository_factory(data_dir_path):
+def faa_data_repository_factory(data_dir_path: str) -> FAADataRepository:
     filenames = os.listdir(data_dir_path)
     pd_data = {}
     for filename in filenames:
@@ -18,7 +20,7 @@ class FAADataRepository:
     def __init__(self, data):
         self.pq_data = data
 
-    def get_info(self):
+    def get_info(self) -> pd.DataFrame:
         info_dict = {'data_file_name': [], 'column_list': [], 'total_number_of_rows': []}
         for filename, data in self.pq_data.items():
             df = data.to_pandas()
@@ -27,11 +29,11 @@ class FAADataRepository:
             info_dict['total_number_of_rows'].append(len(df.index))
         return pd.DataFrame.from_dict(info_dict)
 
-    def get_aircraft_models(self):
+    def get_aircraft_models(self) -> pd.DataFrame:
         models_df = self.pq_data['aircraft_models.parquet'].to_pandas()
         return models_df.filter(['model', 'manufacturer', 'seats'], axis=1)
 
-    def get_aircrafts_filtered(self, manufacturer=None, model=None):
+    def get_aircrafts_filtered(self, manufacturer: str = None, model: str = None) -> pd.DataFrame:
         df = self._get_active_aircraft_with_models_joined()
         df = df[['model', 'manufacturer', 'seats', 'aircraft_serial', 'name', 'state']]
         if manufacturer:
@@ -41,11 +43,11 @@ class FAADataRepository:
         df.reset_index(drop=True, inplace=True)
         return df
 
-    def get_report(self):
+    def get_report(self) -> pd.DataFrame:
         df = self._get_active_aircraft_with_models_joined()
         return df.groupby(['manufacturer', 'model', 'state']).size().reset_index(name='count')
 
-    def get_report_pivot(self):
+    def get_report_pivot(self) -> pd.DataFrame:
         df = self._get_active_aircraft_with_models_joined()
         summary_counts = df.groupby(['manufacturer', 'model', 'state']).size().reset_index(name='count')
         pivot_df = summary_counts.pivot(index=['manufacturer', 'model'], columns='state', values='count')
@@ -53,14 +55,14 @@ class FAADataRepository:
         pivot_df.reset_index(inplace=True)
         return pivot_df
 
-    def get_data_by_sql_string(self, sql_string):
+    def get_data_by_sql_string(self, sql_string: str) -> pd.DataFrame:
         dataframes = {}
         for name, data in self.pq_data.items():
             name, _ = name.split('.')
             dataframes[name] = data.to_pandas()
         return sqldf(sql_string, dataframes)
 
-    def _get_active_aircraft_with_models_joined(self):
+    def _get_active_aircraft_with_models_joined(self) -> pd.DataFrame:
         models_df = self.pq_data['aircraft_models.parquet'].to_pandas()
         aircraft_df = self.pq_data['aircraft.parquet'].to_pandas()
         active_aircraft_df = aircraft_df.loc[aircraft_df['status_code'] == 'A']
